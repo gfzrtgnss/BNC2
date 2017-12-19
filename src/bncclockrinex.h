8c8b96b9d1d34b1f67d14d197d802a404c685b2b
gfzrtgnss <jxinyuan@gfz-potsdam.de>
2017-12-19 15:12:21 +0100

    xinyuan

---
+++ src/bncclockrinex.h
@@ -0,0 +1,22 @@
+#ifndef BNCCLOCKRINEX_H
+#define BNCCLOCKRINEX_H
+
+#include <fstream>
+#include <newmat.h>
+#include <QtCore>
+
+#include "bncoutf.h"
+
+class bncClockRinex : public bncoutf {
+ public:
+  bncClockRinex(const QString& sklFileName, const QString& intr, int sampl);
+  virtual ~bncClockRinex();
+  virtual t_irc write(int GPSweek, double GPSweeks, const QString& prn, 
+                      double sp3Clk);
+
+ private:
+  virtual void writeHeader(const QDateTime& datTim);
+  bool _append;
+};
+
+#endif
